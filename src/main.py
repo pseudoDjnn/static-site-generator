e7ca2eceafc8ba2c3e7274a9993a63e0ebe44eb0
pseudoDjnn <lavahrexes@gmail.com>
2025-03-23 23:14:11 -0400

    no pycache, add src folder and files, add bash script to run in CLI

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode, TextType
+
+def main():
+    #  Create a test TextNode ( for example, a link)
+    node = TextNode("This is some anchor text", TextType.LINK, "https://boot.dev")
+    #  Print the node to see its string representation
+    print(node)
+
+if __name__=="__main__":
+    main()
