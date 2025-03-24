1be48e35a3688905a122c36209405c4f56a926ba
pseudoDjnn <lavahrexes@gmail.com>
2025-03-24 14:05:47 -0400

    update props_to_html method

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -4,3 +4,19 @@ class HTMLNode:
       self.value = value
       self.children = children
       self.props = props
+      
+    def to_html(self):
+      raise NotImplementedError
+    
+    def props_to_html(self):
+      if not self.props:  #  Check for None or empty dictionary
+        return ""
+      
+      # Use list comphrehension to generate a list of ' key="value"' strings
+      attributes = [f' {key}="{value}' for key, value in self.props.items()]
+      
+      # Join the list into a single string and return it
+      return "".join(attributes)
+    
+    def __repr__(self):
+      pass
