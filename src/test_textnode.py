308f21e337b0706d6210527b25b8e8ac3f3bd4ef
pseudoDjnn <lavahrexes@gmail.com>
2025-03-23 23:53:50 -0400

    add type not equal

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -14,6 +14,12 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node!", TextType.BOLD)
         self.assertNotEqual(node, node2)
         
+    def test_not_eq_different_type(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+        
+        
 
 if __name__=="__main__":
     unittest.main()
