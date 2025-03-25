67eae4f4cbe61d6c573d863d543e9bcc29865744
pseudoDjnn <lavahrexes@gmail.com>
2025-03-24 22:20:19 -0400

    update to tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -7,7 +7,16 @@ class TestHTMLNode(unittest.TestCase):
   def test_props_to_html(self):
     # Test case 1: No properties
     node1 = HTMLNode()
-    assert node1.props_to_html() == ""
+    self.assertEqual(node1.props_to_html(), "")
+    
+    node2 = HTMLNode(props={'href': "https://example.com"})
+    self.assertEqual(node2.props_to_html(), ' href="https://example.com"')
+    
+    node3 = HTMLNode(props={"href": "https://example.com", "target": "_blank"})
+    self.assertEqual(node3.props_to_html(), ' href="https://example.com" target="_blank"')
+
+    node4 = HTMLNode(props={"data-test": ""})
+    self.assertEqual(node4.props_to_html(), ' data-test=""')
 
   def test_repr(self):
     pass
