38e4737883306f3e5b8370cb146abd4d4bf569e7
pseudoDjnn <lavahrexes@gmail.com>
2025-03-24 23:04:03 -0400

    finished tests for htmlnode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -9,17 +9,32 @@ class TestHTMLNode(unittest.TestCase):
     node1 = HTMLNode()
     self.assertEqual(node1.props_to_html(), "")
     
+    # Test case 2: Single property
     node2 = HTMLNode(props={'href': "https://example.com"})
     self.assertEqual(node2.props_to_html(), ' href="https://example.com"')
     
+    # Test case 3: Multiple properties
     node3 = HTMLNode(props={"href": "https://example.com", "target": "_blank"})
     self.assertEqual(node3.props_to_html(), ' href="https://example.com" target="_blank"')
 
+    # Test case 4: Empty string value
     node4 = HTMLNode(props={"data-test": ""})
     self.assertEqual(node4.props_to_html(), ' data-test=""')
 
   def test_repr(self):
-    pass
+    # Test case 1: Empty node
+    node1 = HTMLNode()
+    self.assertEqual(
+      repr(node1),
+      "HTMLNode(tag=None, value=None, children=0, props=None)"
+    )
+    
+    # Test case 2: Node with tag, value, props
+    node2 = HTMLNode(tag="p", value="Hello World", props={"class": "exmaple-class"})
+    self.assertEqual(
+      repr(node2),
+      "HTMLNode(tag='p', value='Hello World', children=0, props={'class': 'exmaple-class'})"
+    )
 
 if __name__=="__main__":
     unittest.main()
