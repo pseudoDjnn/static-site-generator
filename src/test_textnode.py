3c519c6d0dfaee60d8b2f66f34ad5cea7e7ae49b
pseudoDjnn <lavahrexes@gmail.com>
2025-03-28 12:41:37 -0400

    just added new tests and will set them up when I have time

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -33,6 +33,24 @@ class TestTextNode(unittest.TestCase):
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
+    
+    def test_bold(self):
+        pass
+    
+    def test_italic(self):
+        pass
+    
+    def test_code(self):
+        pass
+    
+    def test_link(self):
+        pass
+    
+    def test_image(self):
+        pass
+    
+    def test_invalid_type(self):
+        pass
         
 
 if __name__=="__main__":
