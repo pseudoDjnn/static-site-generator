e25c80fd5ae39567657a063e19a2b1e579543a03
pseudoDjnn <lavahrexes@gmail.com>
2025-04-01 13:27:10 -0400

    both tests work now

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -17,20 +17,20 @@ class TestInlineMarkdown(unittest.TestCase):
       new_nodes
     )
   
-  # def test_delimiter_bold_double(self):
-  #   node = TextNode(
-  #     "This is text with a **bold** word and **another**", TextType.TEXT
-  #   )
-  #   new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
-  #   self.assertEqual(
-  #     [
-  #       TextNode("This is text with a ", TextType.TEXT),
-  #       TextNode("bold", TextType.BOLD),
-  #       TextNode(" word and ", TextType.TEXT),
-  #       TextNode("another", TextType.BOLD)
-  #     ],
-  #     new_nodes
-  #   )
+  def test_delimiter_bold_double(self):
+    node = TextNode(
+      "This is text with a **bold** word and **another**", TextType.TEXT
+    )
+    new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+    self.assertEqual(
+      [
+        TextNode("This is text with a ", TextType.TEXT),
+        TextNode("bold", TextType.BOLD),
+        TextNode(" word and ", TextType.TEXT),
+        TextNode("another", TextType.BOLD)
+      ],
+      new_nodes
+    )
   
 if __name__=="__main__":
   unittest.main()
