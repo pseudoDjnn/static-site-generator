8d1975352424af60c31a3acca8e55d9616dc1770
pseudoDjnn <lavahrexes@gmail.com>
2025-04-01 13:10:40 -0400

    indentation error fixed

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -17,5 +17,5 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         split_nodes.append(TextNode(sections[i], TextType.TEXT))
       else:
         split_nodes.append(TextNode(sections[i], text_type))
-      new_nodes.extend(split_nodes)
+    new_nodes.extend(split_nodes)
   return new_nodes
