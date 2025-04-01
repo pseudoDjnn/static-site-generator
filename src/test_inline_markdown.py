72df38bc5c2755591abe83035325e354b45e63c3
pseudoDjnn <lavahrexes@gmail.com>
2025-04-01 13:06:16 -0400

    error in logic form last night

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -5,7 +5,32 @@ from inline_markdown import split_nodes_delimiter
 from textnode import TextNode, TextType
 
 class TestInlineMarkdown(unittest.TestCase):
-  pass
+  def test_delimiter_bold(self):
+    node = TextNode("This is text with a **bold** word", TextType.TEXT)
+    new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+    self.assertEqual(
+      [
+        TextNode("This is text with a ", TextType.TEXT),
+        TextNode("bold", TextType.BOLD),
+        TextNode(" word", TextType.TEXT)
+      ],
+      new_nodes
+    )
+  
+  # def test_delimiter_bold_double(self):
+  #   node = TextNode(
+  #     "This is text with a **bold** word and **another**", TextType.TEXT
+  #   )
+  #   new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+  #   self.assertEqual(
+  #     [
+  #       TextNode("This is text with a ", TextType.TEXT),
+  #       TextNode("bold", TextType.BOLD),
+  #       TextNode(" word and ", TextType.TEXT),
+  #       TextNode("another", TextType.BOLD)
+  #     ],
+  #     new_nodes
+  #   )
   
 if __name__=="__main__":
   unittest.main()
