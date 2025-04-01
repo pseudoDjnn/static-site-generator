6f73770dadd021b1a5f644954e618a4127751e50
pseudoDjnn <lavahrexes@gmail.com>
2025-03-31 21:42:24 -0400

    tests are all working

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -63,19 +63,52 @@ class TestHTMLNode(unittest.TestCase):
     )
   
   def test_leaf_to_html_no_tag(self):
-    pass
+    node = LeafNode(None, "Hello World")
+    self.assertEqual(node.to_html(), "Hello World")
   
-  def tets_to_html_with_children(self):
-    pass
+  def test_to_html_with_children(self):
+    child_node = LeafNode("span", "child")
+    parent_node = ParentNode("div", [child_node])
+    self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
   
   def test_to_html_with_grandchildren(self):
-    pass
+    grandchild_node = LeafNode("b", "grandchild")
+    child_node = ParentNode("span", [grandchild_node])
+    parent_node = ParentNode("div", [child_node])
+    self.assertEqual(
+      parent_node.to_html(),
+      "<div><span><b>grandchild</b></span></div>"
+      )
   
   def test_to_html_many_children(self):
-    pass
+    node = ParentNode(
+      "p",
+      [
+        LeafNode("b", "Bold text"),
+        LeafNode(None, "Normal text"),
+        LeafNode("i", "Italic text"),
+        LeafNode(None, "Normal text")
+      ]
+    )
+    self.assertEqual(
+      node.to_html(),
+      "<p><b>Bold text</b>Normal text<i>Italic text</i>Normal text</p>"
+    )
   
-  def tets_headings(self):
-    pass
+  def test_headings(self):
+    node = ParentNode(
+      "h2",
+      [
+        LeafNode("b", "Bold text"),
+        LeafNode(None, "Normal text"),
+        LeafNode("i", "Italic text"),
+        LeafNode(None, "Normal text")
+      ]
+    )
+    self.assertEqual(
+      node.to_html(),
+      "<h2><b>Bold text</b>Normal text<i>Italic text</i>Normal text</h2>"
+    )
 
 if __name__=="__main__":
     unittest.main()
