b064d7c13c08af79aa48900da61920051cb9edba
pseudoDjnn <lavahrexes@gmail.com>
2025-03-31 21:57:44 -0400

    back to where I left off now

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,4 +1,10 @@
 from textnode import TextNode, TextType
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-  pass
+  new_nodes = []
+  for old_node in old_nodes:
+    if old_node.text_type != TextType.TEXT:
+      new_nodes.append(old_node)
+      continue
+    
+  return new_nodes
