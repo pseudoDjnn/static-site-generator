45096bca3859681f6209ce270251ce1bfcda7f29
pseudoDjnn <lavahrexes@gmail.com>
2025-04-02 12:20:52 -0400

    completed tests and they all work

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -46,6 +46,43 @@ class TestInlineMarkdown(unittest.TestCase):
       ],
       new_nodes
     )
+    
+  def test_delimiter_italic(self):
+    node= TextNode("This is text with an _italic_ word", TextType.TEXT)
+    new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
+    self.assertEqual(
+      [
+        TextNode("This is text with an ", TextType.TEXT),
+        TextNode("italic", TextType.ITALIC),
+        TextNode(" word", TextType.TEXT)
+      ],
+      new_nodes
+    )
+  
+  def test_delimiter_bold_and_italic(self):
+    node = TextNode("**bold** and _italic_", TextType.TEXT)
+    new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+    new_nodes = split_nodes_delimiter(new_nodes, "_", TextType.ITALIC)
+    self.assertEqual(
+      [
+        TextNode("bold", TextType.BOLD),
+        TextNode(" and ", TextType.TEXT),
+        TextNode("italic", TextType.ITALIC),
+      ],
+      new_nodes
+    )
+
+  def test_delimiter_code(self):
+    node = TextNode("This is text with as a `code block` word", TextType.TEXT)
+    new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+    self.assertEqual(
+      [
+       TextNode("This is text with as a ", TextType.TEXT),
+       TextNode("code block", TextType.CODE),
+       TextNode(" word", TextType.TEXT)
+      ],
+      new_nodes
+    )
   
 if __name__=="__main__":
   unittest.main()
