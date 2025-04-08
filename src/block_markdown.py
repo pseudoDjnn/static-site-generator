f84f244437c4e9340fd2aa87f39be900864c0d5d
pseudoDjnn <lavahrexes@gmail.com>
2025-04-08 18:03:13 -0400

    setting up the checks with two left

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -10,9 +10,24 @@ class BlockType(Enum):
     UNORDERED_LIST = "unordered_list"
     ORDERED_LIST = "ordered_list"
     
-    
+# Step 2: Define the function to check the Enums
 def block_to_block_type(block):
-    pass
+    # Check 1: If it is a heading (1-6 '#' followed by a space)
+    if block.startswidth("#"):
+        return BlockType.HEADING
+    
+    # Check 2: If it is a code block (starts and ends with ```)
+    if block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    
+    # Check 3: If it is a quote block (every line starts with '>')
+    if all(line.startswith(">") for line in block.split("\n")):
+        return BlockType.QUOTE
+    
+    
+    
+    # If none of the above, it's a paragraph
+    return BlockType.PARAGRAPH
 
 def markdown_to_blocks(markdown):
   # Split the markdown by double newlines
