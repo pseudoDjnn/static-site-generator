c66d3d557c1c3ecc30f70c43c41c2becd9f6af06
pseudoDjnn <lavahrexes@gmail.com>
2025-04-07 23:19:18 -0400

    added test_block_markdown.py

---
+++ src/test_block_markdown.py
@@ -0,0 +1,11 @@
+import unittest
+from block_markdown import markdown_to_blocks
+
+
+class TestBlockMarkdown(unittest.TestCase):
+    def test_makrdown_to_blocks(self):
+        pass
+
+
+if __name__=="__main__":
+    unittest.main()
