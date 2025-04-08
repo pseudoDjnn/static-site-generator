221b2e20efcf4d486fdd4e9d1ba4b26f7f532584
pseudoDjnn <lavahrexes@gmail.com>
2025-04-08 18:27:46 -0400

    finished the Enum checks and now onto tests

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -13,7 +13,7 @@ class BlockType(Enum):
 # Step 2: Define the function to check the Enums
 def block_to_block_type(block):
     # Check 1: If it is a heading (1-6 '#' followed by a space)
-    if block.startswidth("#"):
+    if block.startswith("#"):
         return BlockType.HEADING
     
     # Check 2: If it is a code block (starts and ends with ```)
@@ -24,7 +24,19 @@ def block_to_block_type(block):
     if all(line.startswith(">") for line in block.split("\n")):
         return BlockType.QUOTE
     
-    
+    # Check 4: If it is an unordered list (every line starts with '- ')
+    if all(line.startswith("- ") for line in block.split("\n")):
+        return BlockType.UNORDERED_LIST
+
+    # Check if it's an ordered list (lines start with '1. ', '2. ', '3. ', etc...)
+    lines = block.split("\n")
+    expected_number = 1
+    for line in lines:
+        if not line.startswith(f"{expected_number}. "):
+            break
+        expected_number += 1
+    else:
+        return BlockType.ORDERED_LIST
     
     # If none of the above, it's a paragraph
     return BlockType.PARAGRAPH
