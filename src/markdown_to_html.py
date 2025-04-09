78da293c2e76cd4306e7b01bd9bdf10b57d15a1c
pseudoDjnn <lavahrexes@gmail.com>
2025-04-08 23:06:43 -0400

    add helper function for markdown_to_html_node

---
+++ src/markdown_to_html.py
@@ -0,0 +1,8 @@
+from htmlnode import HTMLNode
+from block_markdown import markdown_to_blocks, block_to_block_type
+
+def create_paragraph_node(text):
+    pass
+
+def markdown_to_html_node(markdown):
+    pass
