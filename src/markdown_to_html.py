20a514deac67eebf5face40d2e201a19982a4484
pseudoDjnn <lavahrexes@gmail.com>
2025-04-09 12:43:45 -0400

    commit from last night

--- src/markdown_to_html.py
+++ src/markdown_to_html.py
@@ -1,8 +1,14 @@
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, ParentNode, LeafNode
 from block_markdown import markdown_to_blocks, block_to_block_type
 
 def create_paragraph_node(text):
-    pass
+    return ParentNode(tag="p", children=[LeafNode(tag=None, value=text)])
 
 def markdown_to_html_node(markdown):
-    pass
+    blocks = markdown_to_blocks(markdown)
+    all_nodes = []
+
+    for block in blocks:
+        pass
+
+    return ParentNode(tag="div", children=all_nodes)
