0cd0cc2e3ca256ed6e617406cb1e184b6f73da9d
pseudoDjnn <lavahrexes@gmail.com>
2025-04-09 13:02:16 -0400

    starting with the logic from last night

--- src/markdown_to_html.py
+++ src/markdown_to_html.py
@@ -1,5 +1,5 @@
 from htmlnode import HTMLNode, ParentNode, LeafNode
-from block_markdown import markdown_to_blocks, block_to_block_type
+from block_markdown import markdown_to_blocks, block_to_block_type, BlockType
 
 def create_paragraph_node(text):
     return ParentNode(tag="p", children=[LeafNode(tag=None, value=text)])
@@ -9,6 +9,13 @@ def markdown_to_html_node(markdown):
     all_nodes = []
 
     for block in blocks:
-        pass
+        block_type = block_to_block_type(block)
+        if block_type == BlockType.PARAGRAPH:
+            node = create_paragraph_node(block)
+
+        else:
+            continue
+        
+        all_nodes.append(node)
 
     return ParentNode(tag="div", children=all_nodes)
