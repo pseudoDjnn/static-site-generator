942091f48f1adae0b1503d6656ef0ad4030988e7
pseudoDjnn <lavahrexes@gmail.com>
2025-04-14 14:22:47 -0400

    update test_code

--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -148,9 +148,19 @@ This is a paragraph text
             "<div><blockquote>This is a blockquote block</blockquote><p>This is a paragraph text</p></div>"
         )
 
-    
     def test_code(self):
-        pass
+        md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```        
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>"
+        )
 
 if __name__ == "__main__":
     unittest.main()
