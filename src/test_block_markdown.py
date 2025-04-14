9cf2d84a457fdc14a4b3dc881458c29d4bdfedf9
pseudoDjnn <lavahrexes@gmail.com>
2025-04-13 21:07:08 -0400

    setting up other tests

--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -79,9 +79,36 @@ tag here
         self.assertEqual(
             html,
             "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p></div>",
-        )    
-    
+        )
+          
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
 
+This is another paragraph with _italic_ text and `code` here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+        
+    def test_lists(self):
+        pass
+    
+    def test_headings(self):
+        pass
+    
+    def test_blockquote(self):
+        pass
+    
+    def test_code(self):
+        pass
 
 if __name__ == "__main__":
     unittest.main()
