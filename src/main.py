527cd37e393aee152deb5174d3ab259ca6b7ff21
pseudoDjnn <lavahrexes@gmail.com>
2025-04-15 15:01:48 -0400

    updated small logical errors and now starting to generate pages

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 from textnode import TextNode, TextType
 
+def extract_title(markdown):
+    pass
+
 def main():
     #  Create a test TextNode ( for example, a link)
     node = TextNode("This is some anchor text", TextType.LINK, "https://boot.dev")
