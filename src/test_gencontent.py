47fb8cc3e9b79d6b9c0e7657ae8085b789762131
pseudoDjnn <lavahrexes@gmail.com>
2025-04-16 12:50:54 -0400

    setting up tests for gencontent

---
+++ src/test_gencontent.py
@@ -0,0 +1,13 @@
+import unittest
+
+from gencontent import extract_title
+
+class TestExtractTitle(unittest.TestCase):
+    def test_eq(self):
+        pass
+    
+    def test_eq_double(self):
+        pass
+
+if __name__=="__main__":
+    unittest.main()
