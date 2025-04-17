a7fff0005a22cb7868b5f50c5a1454d39d3b201b
pseudoDjnn <lavahrexes@gmail.com>
2025-04-17 12:10:58 -0400

    starting generate_pages func

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,3 +1,9 @@
+import os
+from markdown_blocks import markdown_to_html_node
+
+def generate_pages(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+
 def extract_title(markdown):
     # Break the markdown into individual lines
     lines = markdown.split("\n")
