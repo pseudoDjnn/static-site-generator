fb93e3b485f25e46ba49b83d7c2f75a92c7ac429
pseudoDjnn <lavahrexes@gmail.com>
2025-04-17 12:44:09 -0400

    update generate_pages func

--- src/gencontent.py
+++ src/gencontent.py
@@ -3,6 +3,16 @@ from markdown_blocks import markdown_to_html_node
 
 def generate_pages(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    from_file = open(from_path, "r")
+    markdown_content = from_file.read()
+    from_file.close()
+
+    template_file = open(template_path, "r")
+    template = template_file.read()
+    template_file.close()
+
+    node = markdown_to_html_node(markdown_content)
+    html = node.to_html()
 
 def extract_title(markdown):
     # Break the markdown into individual lines
