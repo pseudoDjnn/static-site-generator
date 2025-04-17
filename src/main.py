ae76f65a5d254029a098d29897dcbee12b239dfe
pseudoDjnn <lavahrexes@gmail.com>
2025-04-17 13:37:51 -0400

    updating main.py now

--- src/main.py
+++ src/main.py
@@ -3,8 +3,15 @@ import shutil
 
 from copystatic import copy_files_recursive
 
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
+
 def main():
-    print("This works")
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
     
 
 if __name__=="__main__":
