a1bf40f0c00f4fe4107e8477d12a7522e42171fc
pseudoDjnn <lavahrexes@gmail.com>
2025-04-17 22:50:19 -0400

    update main.py

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import os
 import shutil
 
 from copystatic import copy_files_recursive
+from gencontent import generate_pages
 
 dir_path_static = "./static"
 dir_path_public = "./public"
@@ -12,6 +13,16 @@ def main():
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
+        
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+    print("Generating page...")
+    generate_pages(
+        os.path.join(dir_path_content, "index.md"),
+        template_path,
+        os.path.join(dir_path_public, "index.html")
+    )
     
 
 if __name__=="__main__":
