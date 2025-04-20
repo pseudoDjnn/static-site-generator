33a712aa2ed4cb074c4f6a5d909fe6b9b3f48f5c
pseudoDjnn <lavahrexes@gmail.com>
2025-04-20 16:51:22 -0400

    updating for deployment

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 import shutil
 
 from copystatic import copy_files_recursive
@@ -9,6 +9,12 @@ dir_path_public = "./public"
 dir_path_content = "./content"
 template_path = "./template.html"
 
+basepath = sys.argv[1]
+
+if len(basepath) != 1:
+    default = "/"
+    sys.exit()
+
 def main():
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
@@ -18,7 +24,7 @@ def main():
     copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating content...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
     
 if __name__=="__main__":
     main()
